51dd51ab95d36dc12540dc6d5508ccfb8192aa7e
Bright Anyawe <brightanyawe55@gmail.com>
2024-11-08 05:46:49 +0000

    Refactor: change the path of the images

---
+++ .history/src/Components/Layers/AppPresentation_20241108053723.jsx
@@ -0,0 +1,252 @@
+export const AppPresentation = ({
+  appPresentationTextColor,
+  appPresentationHeaderText,
+  appInfoSliderColor
+}) => {
+  return (
+    <>
+      <section
+        className="appPresentationContainer"
+        data-testid="appPresentation-contents"
+      >
+        <div className="appPresentation">
+          <div
+            className="appPresentationSubTitle"
+            ref={appPresentationHeaderText}
+          >
+            <h3 className="appPresentationTitle">
+              Enjoy your financial freedom with a single app.
+            </h3>
+
+            <p className="appPresentationPara">
+              All you need is <span className="appTextDesign">Parap</span>
+            </p>
+          </div>
+
+          <div
+            className="appPresentationContents mobileContents"
+            ref={appPresentationTextColor}
+          >
+            <div className="iphoneContainer">
+              <img
+                className="leftVector"
+                src="public/Figma Img/Logo/Vector 1.png"
+                alt=""
+              />
+              <img
+                className="iphoneImg"
+                src="public/Figma Img/Logo/iPhone 14 Pro Space Black Mockup (2).png"
+                alt="iPhone 14 Pro Space Black Mockup"
+              />
+              <img
+                className="rightVector"
+                src="public/Figma Img/Logo/Vector 1.png"
+                alt=""
+              />
+            </div>
+
+            <div className="appInfoSlider" ref={appInfoSliderColor}>
+              <div className="accountLeftContents">
+                <div className="accountContainer">
+                  <div
+                    className="accountTextContainer"
+                    ref={appPresentationTextColor}
+                  >
+                    <h4>Open an account</h4>
+                    <p>
+                      Open your account in just few circles, anywhere anytime
+                    </p>
+                  </div>
+
+                  <img
+                    src="public/Figma Img/Logo/UserAccount.png"
+                    alt="UserAccount"
+                  />
+                </div>
+                <div className="sendMoneyContainer">
+                  <div className="sendMoneyTextContainer">
+                    <h4>Spend debt free</h4>
+                    <p>
+                      Know where your money goes. Track your spendings with
+                      charts in real time.
+                    </p>
+                  </div>
+
+                  <img
+                    src="public/Figma Img/Logo/Send money circle.png"
+                    alt="Send money circle"
+                  />
+                </div>
+                <div className="earnCashBackFill">
+                  <div className="earnCashBackTextContainer">
+                    <h4 className="mobileCashBackTitle">Earn cashback</h4>
+                    <p>
+                      Earn instant cashback as you spend. No minimum spending
+                      limits or complicated rules.
+                    </p>
+                  </div>
+
+                  <img
+                    src="public/Figma Img/Logo/Cashback Fill.png"
+                    alt="earnCashBackImage"
+                  />
+                </div>
+              </div>
+
+              <div className="accountRightContents">
+                <div className="saveMoneyContainer">
+                  <img
+                    src="public/Figma Img/Logo/Save money circle.png"
+                    alt="saveImage"
+                  />
+
+                  <div className="saveMoneyTextContainer">
+                    <h4>Save Money</h4>
+                    <p>Open saving account,save money regularly.</p>
+                  </div>
+                </div>
+                <div className="requestMoneyContainer">
+                  <img
+                    src="public/Figma Img/Logo/Request circle.png"
+                    alt="requestImage"
+                  />
+
+                  <div className="requestMoneyTextContainer">
+                    <h4>Request money</h4>
+                    <p>
+                      Select a contact, set the amount, add your message, and
+                      send your request.
+                    </p>
+                  </div>
+                </div>
+                <div className="qRCode">
+                  <img
+                    src="public/Figma Img/Logo/QR circle.png"
+                    alt="qRCodeImage"
+                  />
+
+                  <div className="qRCodeTextContainer">
+                    <h4>Pay & Receive Payments with QR</h4>
+                    <p>
+                      Make payments by scanning a QR code. Share your QR code to
+                      receive instant payments.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            className="appPresentationContents desktopContents"
+            ref={appPresentationTextColor}
+          >
+            <div className="accountLeftContents">
+              <div className="accountContainer">
+                <div
+                  className="accountTextContainer"
+                  ref={appPresentationTextColor}
+                >
+                  <h4>Open an account</h4>
+                  <p>Open your account in just few circles, anywhere anytime</p>
+                </div>
+
+                <img
+                  src="public/UserAccount.png"
+                  alt="UserAccount"
+                />
+              </div>
+              <div className="sendMoneyContainer">
+                <div className="sendMoneyTextContainer">
+                  <h4>Spend debt free</h4>
+                  <p>
+                    Know where your money goes. Track your spendings with charts
+                    in real time.
+                  </p>
+                </div>
+
+                <img
+                  src="public/Send money circle.png"
+                  alt="Send money circle"
+                />
+              </div>
+              <div className="earnCashBackFill">
+                <div className="earnCashBackTextContainer">
+                  <h4>Earn cashback</h4>
+                  <p>
+                    Earn instant cashback as you spend. No minimum spending
+                    limits or complicated rules.
+                  </p>
+                </div>
+
+                <img
+                  src="public/Figma Img/Logo/Cashback Fill.png"
+                  alt="earnCashBackImage"
+                />
+              </div>
+            </div>
+            <div className="iphoneContainer">
+              <img
+                className="desktopLeftVector"
+                src="Figma Img/Logo/Vector 1.png"
+                alt=""
+              />
+              <img
+                className="iphoneImg"
+                src="Figma Img/Logo/iPhone 14 Pro Space Black Mockup (2).png"
+                alt="iPhone 14 Pro Space Black Mockup"
+              />
+              <img
+                className="desktopRightVector"
+                src="Figma Img/Logo/Vector 1.png"
+                alt=""
+              />
+            </div>
+
+            <div className="accountRightContents">
+              <div className="saveMoneyContainer">
+                <img
+                  src="Figma Img/Logo/Save money circle.png"
+                  alt="saveImage"
+                />
+
+                <div className="saveMoneyTextContainer">
+                  <h4>Save Money</h4>
+                  <p>Open saving account,save money regularly.</p>
+                </div>
+              </div>
+              <div className="requestMoneyContainer">
+                <img
+                  src="Figma Img/Logo/Request circle.png"
+                  alt="requestImage"
+                />
+
+                <div className="requestMoneyTextContainer">
+                  <h4>Request money</h4>
+                  <p>
+                    Select a contact, set the amount, add your message, and send
+                    your request.
+                  </p>
+                </div>
+              </div>
+              <div className="qRCode">
+                <img
+                  src="public/Figma Img/Logo/QR circle.png"
+                  alt="qRCodeImage"
+                />
+
+                <div className="qRCodeTextContainer">
+                  <h4>Pay & Receive Payments with QR</h4>
+                  <p>
+                    Make payments by scanning a QR code. Share your QR code to
+                    receive instant payments.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
---
+++ .history/src/Components/Layers/Header_Responsive_20241107120027.jsx
@@ -0,0 +1,126 @@
+// import { useContext } from "react";
+import { useRef, useState } from "react";
+import PropTypes from "prop-types";
+import { useContext } from "react";
+import { ProvideContext } from "../../App";
+// import HeroContext from "../../ContextProvider/contextProvider";
+
+export const Header = ({
+  bodyRef,
+  heroScreenTxtRef,
+  cardText,
+  appPresentationTextColor,
+  appPresentationHeaderText,
+  cashBackHeadTitle,
+  cardsColor,
+  brandsTextTitleColor,
+  downloadTitleColor,
+}) => {
+  // const bodyRef = useRef(null);
+  const {isBlack, setIsBlack} = useContext(ProvideContext)
+
+  const businessRef = useRef(null);
+  const langTextColor = useRef(null);
+  const menuRef = useRef(null);
+  const logoRef = useRef(null);
+
+  //   const heroScreenTxtRef = useRef(null);
+
+  // const heroScreenTxt = useRef(null);
+  //   const {  businessRef, handleElementStyling } = useContext(HeroContext);
+
+  function handleElementStyling() {
+    const setBackgroundColor = !isBlack;
+    setIsBlack(setBackgroundColor);
+
+    if (setBackgroundColor) {
+      menuRef.current.classList.add("menuRef");
+    } else {
+      menuRef.current.classList.remove("menuRef");
+    }
+  }
+
+  // function handleVisibility() {}
+
+  return (
+    <>
+      <section
+        className="headerContentsContainer"
+        data-testid="header-contents"
+      >
+        <div className="headerContents">
+          <div className="headerRightContent">
+            <div className="Logo">
+              <img
+                onClick={handleElementStyling}
+                src="public/Figma Img/Logo/Group 132.png"
+                alt="logo"
+              />
+              <h2 ref={logoRef}>Parap</h2>
+            </div>
+
+            <div className="groupContainer">
+              <div className="personalContainer">
+                <p>Personal</p>
+                <img
+                  src="public/Figma Img/Logo/arrow-circle-down-stylish.png"
+                  alt="downArrow"
+                />
+                {/* <p>svg</p> */}
+              </div>
+              <div className="BusinessContainer">
+                <p ref={businessRef}>Business</p>
+                <img
+                  src="public/Figma Img/Logo/arrow-circle-down.png"
+                  alt="downArrow"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="menuContainer" ref={menuRef}>
+            <svg
+              className="menu"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <title>menu</title>
+              <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
+            </svg>
+          </div>
+          <div className="headerLeftContent">
+            <div className="buttonsContainer">
+              <button className="loginBtn">Log In</button>
+              <button className="signUpBtn">Sign In</button>
+            </div>
+            <div className="Language">
+              <img src="public/Figma Img/Logo/image 63.png" alt="countryLogo" />
+              <div className="langTextContainer">
+                <p className="langText" ref={langTextColor}>
+                  English
+                </p>
+                <img
+                  src="public/Figma Img/Logo/arrow-circle-down.png"
+                  alt="down Arrow"
+                />
+              </div>
+            </div>
+            <div></div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+Header.propTypes = {
+  bodyRef: PropTypes.object,
+  heroScreenTxtRef: PropTypes.object,
+  cardText: PropTypes.object,
+  appPresentationTextColor: PropTypes.object,
+  appPresentationHeaderText: PropTypes.object,
+  cashBackHeadTitle: PropTypes.object,
+  cardsColor: PropTypes.object,
+  brandsTextTitleColor: PropTypes.object,
+  downloadTitleColor: PropTypes.object,
+};
